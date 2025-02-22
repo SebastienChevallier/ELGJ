8cac061e4e1d7d52ee6bdbb0bd63307d6481d37f
Sebastien chevallier <s.chevallier.pro@gmail.com>
2025-02-22 12:39:08 +0100

    TwitchIntegration

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TwitchIntegration;
+using Unity.VisualScripting;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+    
+}
